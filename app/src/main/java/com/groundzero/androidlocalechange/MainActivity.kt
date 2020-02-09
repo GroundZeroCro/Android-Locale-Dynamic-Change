80f77872052856a40a01e948a4581317260db53b
Mario Dujić <liberasoftware@gmail.com>
2020-02-09 20:56:48 +0100

    renamed variable

--- app/src/main/java/com/groundzero/androidlocalechange/MainActivity.kt
+++ app/src/main/java/com/groundzero/androidlocalechange/MainActivity.kt
@@ -15,7 +15,7 @@ import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity() {
 
-    private val buttons = listOf("hr", "it", "es")
+    private val locales = listOf("hr", "it", "es")
     private var activeButton: String by Delegates.observable("hr", ::dataChanged)
 
     private var persistenceUtils: PersistenceUtils? = null
@@ -23,12 +23,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        set_croatian_button.setOnClickListener { changeLocale(buttons[0]) }
-        set_italian_button.setOnClickListener { changeLocale(buttons[1]) }
-        set_spanish_button.setOnClickListener { changeLocale(buttons[2]) }
+        set_croatian_button.setOnClickListener { changeLocale(locales[0]) }
+        set_italian_button.setOnClickListener { changeLocale(locales[1]) }
+        set_spanish_button.setOnClickListener { changeLocale(locales[2]) }
         persistenceUtils = PersistenceUtils(this).also {
             activeButton = if (it.getValue(LOCALE_KEY)!! == "") {
-                buttons[0]
+                locales[0]
             } else {
                 it.getValue(LOCALE_KEY)!!
             }
@@ -42,22 +42,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun dataChanged(property: KProperty<*>, oldValue: String, newValue: String) {
         when (oldValue) {
-            buttons[0] -> set_croatian_button.isChecked = false
-            buttons[1] -> set_italian_button.isChecked = false
-            buttons[2] -> set_spanish_button.isChecked = false
+            locales[0] -> set_croatian_button.isChecked = false
+            locales[1] -> set_italian_button.isChecked = false
+            locales[2] -> set_spanish_button.isChecked = false
         }
         when (newValue) {
-            buttons[0] -> {
+            locales[0] -> {
                 set_croatian_button.isChecked = true
-                persistenceUtils!!.setValue(LOCALE_KEY, buttons[0])
+                persistenceUtils!!.setValue(LOCALE_KEY, locales[0])
             }
-            buttons[1] -> {
+            locales[1] -> {
                 set_italian_button.isChecked = true
-                persistenceUtils!!.setValue(LOCALE_KEY, buttons[1])
+                persistenceUtils!!.setValue(LOCALE_KEY, locales[1])
             }
-            buttons[2] -> {
+            locales[2] -> {
                 set_spanish_button.isChecked = true
-                persistenceUtils!!.setValue(LOCALE_KEY, buttons[2])
+                persistenceUtils!!.setValue(LOCALE_KEY, locales[2])
             }
         }
     }
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
             persistenceUtils = PersistenceUtils(context)
         }
 
-        val language: String = persistenceUtils!!.getValue(LOCALE_KEY) ?: buttons[0]
+        val language: String = persistenceUtils!!.getValue(LOCALE_KEY) ?: locales[0]
         val locale = Locale(language)
         Locale.setDefault(locale)
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
